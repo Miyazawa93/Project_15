0c51c44444c32eecf2ee0e392583d5ee331f4708
Miyazawa93 <Kristinhoel@hotmail.com>
2015-11-19 12:00:30 +0100

    Got convert to binary from int and vica verca to work

--- Test/UtilityTest.java
+++ Test/UtilityTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 
 public class UtilityTest {
@@ -11,16 +12,18 @@ public class UtilityTest {
 		Du skal kunne sende inn en streng med 0 og 1 (kun), heretter kalt bitstreng, og få returnert
 		korresponderende int verdi
 		Du skal kunne sende inn en int og få returnert den som en bitstreng med lengde 24*/
+	public Utility utility;	
+	public ExpectedException exception = ExpectedException.none(); 
 	
 	@Before
 	public void setUp (){
-		
+		utility = new Utility();	
 	}
-	public Utility utility = new Utility();	
+	
 	
 	@Test
 	public void testLengthOfString_shouldReturnNull(){
-		assertEquals(0, utility.isItEmpty(""));		
+		assertEquals("0", utility.isItEmpty(""));		
 	}
 	@Test
 	public void testLengthOfString() {
@@ -30,5 +33,17 @@ public class UtilityTest {
 	@Test  (expected = IllegalArgumentException.class)
 	public void testLengthOfString_LongerThan24_IllegalArgumentException(){
 		utility.getLength("010101010101010101010101011101"); 
+		exception.expectMessage("Number of bits exceeds maximum allowed");	
+	}
+	@Test
+	public void convertBinaryToInt(){
+		assertEquals(0, utility.convertBinaryToInt("0")); 
+		assertEquals(13, utility.convertBinaryToInt("1101")); 
+		assertEquals(213, utility.convertBinaryToInt("11010101"));
+	}
+	@Test
+	public void convertIntToBinary(){
+		assertEquals("000000000000000000000000", utility.convertIntToBinary(0)); 
+		assertEquals("000000000000000011010101", utility.convertIntToBinary(213)); 
 	}
 }
--- src/Utility.java
+++ src/Utility.java
@@ -1,21 +1,38 @@
 
 public class Utility {
-	public final int MAX_BIT_LENGTH = 24; 
+	public static final int MAX_BIT_LENGTH = 24;
 
 	public boolean getLength(String string) {
 		if(string.length() < MAX_BIT_LENGTH ){
 			return true; 
 		}
-		 else if(string.length() > 24){
-			throw new IllegalArgumentException(); 
+		else if(string.length() > MAX_BIT_LENGTH){
+			throw new IllegalArgumentException("Number of bits exceeds maximum allowed"); 
 		}
 		return false; 
-}
+	}
 
-	public Object isItEmpty(String string) {
+	public static String isItEmpty(String string) {
 		if(string.length() == 0){
-			return 0; 
+			return "0"; 
 		}
 		return null;
+	}
+
+	public static int convertBinaryToInt(String binary){
+		char [] numbers = binary.toCharArray(); 
+		int converted = 0; 
+		for(int i = numbers.length -1; i >= 0; i--)
+			if(numbers[i]=='1')
+				converted += Math.pow(2, (numbers.length-i - 1));
+		return converted;
+	}
+
+	public static String convertIntToBinary(int integer) {
+		StringBuilder string = new StringBuilder(""); 
+		for(int i = 0; i < MAX_BIT_LENGTH; i++, integer/=2)
+			string.append(integer % 2); 
+		String converted = string.reverse().toString(); 
+		return converted;		
 	}
 }
